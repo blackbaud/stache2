cc43d9938524b7c3a6f779b83cac70354313310b
Blackbaud-TimPepper <tim.pepper@blackbaud.com>
2019-03-05 09:04:58 -0500

    remove unused import

--- src/app/public/src/modules/nav/nav.component.ts
+++ src/app/public/src/modules/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { StacheNavLink } from './nav-link';
 import { StacheNav } from './nav';
 import { StacheRouteService } from '../shared';
